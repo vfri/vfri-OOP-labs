85e5957962f812658551107d643293a09c02a7e9
vfrimarch <matembaum18@yandex.ru>
2018-03-30 21:38:17 +0300

    test for modificate added

--- lab021/lab021/ModivecTest.cpp
+++ lab021/lab021/ModivecTest.cpp
@@ -1,13 +1,37 @@
 #include "stdafx.h"
 #include "Modivec.h"
 
-TEST_CASE("Modivec test")
+TEST_CASE("Modivec test on values validity")
 {
-	CHECK(TrimBlanks("") == "");
-	CHECK(TrimBlanks("Hello") == "Hello");
-	CHECK(TrimBlanks("   Hello") == "Hello");
-	CHECK(TrimBlanks("Hello   ") == "Hello");
-	CHECK(TrimBlanks("    Hello   ") == "Hello");
-	CHECK(TrimBlanks("    ") == "");
-	CHECK(TrimBlanks("A") == "A");
+	std::vector<float> v1 = { -1.0, 3.0 };
+	std::vector<float> v1expRes = { 3.0, 3.0 };
+	Modificate(v1);
+	
+	CHECK(v1 == v1expRes);
+
+	std::vector<float> v2 = { 4, -1, 3, -2, 5, -3 };
+	std::vector<float> v2expRes = { 4, 15, 3, 30, 5, 45 };
+	Modificate(v2);
+
+	CHECK(v2 == v2expRes);
+
+	std::vector<float> v3 = { 1, 2, 3, 4, 5, 6 };
+	std::vector<float> v3expRes = { 1, 2, 3, 4, 5, 6 };
+	Modificate(v3);
+
+	CHECK(v3 == v3expRes);
+
+	std::vector<float> v4 = { -2 };
+	std::vector<float> v4expRes = { -8 };
+	Modificate(v4);
+
+	CHECK(v4 == v4expRes);
+
+	std::vector<float> v5 = { -3.5, -1, -2.0, 3, 4, 6 };
+	std::vector<float> v5expRes = { 73.5, 21, 42, 3, 4, 6 };
+	Modificate(v5);
+
+	CHECK(v5 == v5expRes);
+
 }
+
