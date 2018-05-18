9da750b68e68b6ea79ee5827459bc5d1011b29e3
vfrimarch <matembaum18@yandex.ru>
2018-05-19 01:58:07 +0300

    commit past1805

--- lab024/lab024/GeneratePimeNumberSet.cpp
+++ lab024/lab024/GeneratePimeNumberSet.cpp
@@ -4,18 +4,21 @@
 std::vector<bool> SieveResult(unsigned int bound) // просеивает позиции по методу решета Ёратосфена,
 {	// оставл€€ 1 на позици€х простых чисел
 	std::vector<bool> result(bound + 1, 1);
-	result[0] = result[1] = 0;
-
-	size_t lastPrime = 2;
-	while (lastPrime * lastPrime <= bound)
+	result[0] = 0;
+	if (bound > 0)
 	{
-		for (size_t i = lastPrime * lastPrime; i <= bound; i += lastPrime)
+		result[1] = 0;
+		size_t lastPrime = 2;
+		while (lastPrime * lastPrime <= bound)
 		{
-			result[i] = false;
+			for (size_t i = lastPrime * lastPrime; i <= bound; i += lastPrime)
+			{
+				result[i] = false;
+			}
+			std::vector<bool>::iterator startFindNextPrime = result.begin() + lastPrime + 1;
+			std::vector<bool>::iterator itPrimes = std::find(startFindNextPrime, result.end(), true);
+			lastPrime += std::distance(startFindNextPrime, itPrimes) + 1;
 		}
-		std::vector<bool>::iterator startFindNextPrime = result.begin() + lastPrime + 1;
-		std::vector<bool>::iterator itPrimes = std::find(startFindNextPrime, result.end(), true);
-		lastPrime += std::distance(startFindNextPrime, itPrimes) + 1;
 	}
 	return result;
 }
@@ -25,9 +28,9 @@ std::set<int> GeneratePrimeNumbersSet(unsigned int upperBound)
 	std::vector<bool> primesPositions = SieveResult(upperBound);
 	std::set<int> result;
 	int number = 0;
-	for (auto it = primesPositions.begin(); it != primesPositions.end(); ++it)
+	for (int i = 0; i < primesPositions.size(); ++i)
 	{
-		if (*it)
+		if (primesPositions[i])
 		{
 			result.insert(result.end(), number);
 		}
